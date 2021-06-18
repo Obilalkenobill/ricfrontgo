f7fb240f3bb5bccb682323ef14543188e805700e
Mimouni Bilal <arbreplantebuisson@gmail.com>
2021-06-18 13:37:26 +0200

    'ok'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const path=require('path');
 const app=express();
-app.use(express.static(__dirname+'/dist/src'));
+app.use(express.static(__dirname+'/dist/ric'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/src/index.html'));});
+    res.sendFile(path.join(__dirname+'/dist/ric/index.html'));});
 app.listen(process.env.PORT ||8080);
