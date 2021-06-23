276ff69c1e64369f728561c09a8030e0bd42ed6f
Mimouni Bilal <arbreplantebuisson@gmail.com>
2021-06-23 17:16:51 +0200

    'Ok'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -114,7 +114,7 @@ export function tokenGetter(){
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
-        allowedDomains:["gestion2votefront.herokuapp.com"]
+        allowedDomains:["gestion2vote.herokuapp.com"]
       }
     })
   ],
