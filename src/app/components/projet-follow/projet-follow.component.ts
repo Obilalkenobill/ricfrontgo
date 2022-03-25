d44cfc11cd87002d9bd597e270f5101923ebe1f5
Obilalkenobill <50761645+Obilalkenobill@users.noreply.github.com>
2022-03-25 22:13:40 +0100

    Update projet-follow.component.ts

--- src/app/components/projet-follow/projet-follow.component.ts
+++ src/app/components/projet-follow/projet-follow.component.ts
@@ -9,7 +9,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { Projet } from 'src/app/Models/projet.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { ProjetService } from 'src/app/services/projet.service';
-
+import {MatPaginatorIntl} from '@angular/material/paginator';
 @Component({
   selector: 'app-projet-follow',
   templateUrl: './projet-follow.component.html',
@@ -33,13 +33,15 @@ export class ProjetFollowComponent implements OnInit {
     UserId!:any;
     pageSize!:any;
     pageSizeOptions!:any;
+ itemsPerPageLabel = 'Item par page';
     @ViewChild(MatPaginator) paginator!: MatPaginator;
     @ViewChild(MatSort) sort!: MatSort;
 
     constructor(private projetService: ProjetService,
       public dialog: MatDialog,
       public snackBar: MatSnackBar,
-      private authService: AuthService,private router: Router,  private jwt: JwtHelperService) { }
+      private authService: AuthService,private router: Router,  private jwt: JwtHelperService,private matpag :MatPaginatorIntl) { 
+      matpag.itemsPerPageLabel = 'Items par page';}
 
     ngOnInit(): void {
       this.refresh();
