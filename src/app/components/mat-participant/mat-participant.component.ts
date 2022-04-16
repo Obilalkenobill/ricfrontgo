b6298fcadf34575a4c5abd75e92628a4b8d82ef5
Obilalkenobill <50761645+Obilalkenobill@users.noreply.github.com>
2022-04-16 19:36:10 +0200

    Add files via upload

---
+++ src/app/components/mat-participant/mat-participant.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from 'src/app/Models/user.model';
+
+@Component({
+  selector: 'app-mat-participant',
+  templateUrl: './mat-participant.component.html',
+  styleUrls: ['./mat-participant.component.scss']
+})
+export class MatParticipantComponent implements OnInit {
+ListUsers!:User[];
+  constructor(public dialogRef: MatDialogRef<MatParticipantComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: User[]) {
+      this.ListUsers=data;
+    }
+
+  ngOnInit(): void {
+
+  }
+
+}
