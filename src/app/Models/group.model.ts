5a5d07c2537b548c4aa0d3a7f236178f5869d51d
Obilalkenobill <50761645+Obilalkenobill@users.noreply.github.com>
2022-04-17 18:19:28 +0200

    Add files via upload

--- src/app/Models/group.model.ts
+++ src/app/Models/group.model.ts
@@ -2,10 +2,11 @@ export class Groupe
 {
     id!:number;
     name!:string;
-
+    pers_init_id_id!:number;
     constructor(data: any)
     {
         this.id=data.id;
-this.name=data.name;
-    }
+        this.name=data.name;
+        this.pers_init_id_id=data.pers_init_id_id;
+      }
 }
