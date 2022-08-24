df11a521a6430d67860a3b6ea0aec914885d70e8
unknown <arbreplantebuisson@gmail.com>
2022-08-24 19:33:06 +0200

    OK

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -49,8 +49,9 @@ export class AuthComponent implements OnInit {
 		if (m==="Invalid credentials.")
 		{alert("Mot de passe ou Email invalide.s");}
 		else if (m==="Too many failed login attempts, please try again in 19 minutes.")
-		{alert("Trop de tentative de connection à ce compte, veuillez reéssayer dans 19 minutes.");}
-	}
+		{alert("Trop de tentatives de connexions à ce compte, veuillez reéssayer dans 19 minutes.");}
+		else {alert(m);};	
+}
           },
           (error) =>{
         }
